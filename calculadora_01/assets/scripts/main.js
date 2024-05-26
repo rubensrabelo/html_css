3f2bc67f319b42333180586a85070c1579659bce
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-05-26 11:41:48 -0300

    Ending the calculator script

--- calculadora_01/assets/scripts/main.js
+++ calculadora_01/assets/scripts/main.js
@@ -2,21 +2,59 @@ class Calculator {
     
     constructor(){
         this.display = document.querySelector(".display"); 
-        const meuElemento = document.addEventListener('click', this.meuEventoDeClique);
+        document.addEventListener('click', (e) => this.exibirNoDisplay(e));
     }
 
-    teste() {
-        this.display.value += "5";
-        console.log("5")
-        console.log(this.click);
-    }
-
-    meuEventoDeClique(e) {
+    exibirNoDisplay(e) {
+        const element = e.target.className;
         
-        console.log(e.target.value);
+        if(element.includes("btn")) {
+            let valor = e.target.value;
+            // console.log(value);
+
+            switch(valor) {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                case "%":
+                case ".":
+                    this.display.value += valor;
+                    break;
+                case "+/-":
+                    this.display.value = "-" + this.display.value; 
+                    break;
+                case "clear-all":
+                    this.display.value = "";
+                    break;
+                case "clear-last":
+                    this.display.value = this.display.value.slice(0, -1);
+                    break;
+                default:
+                    let num = this.display.value;
+                    let resultado = this.resultado(valor, num);
+                    this.display.value = resultado
+                    break;
+            }
+        }
+        // console.log(this.display.value);
+    }
+
+    resultado(valor, num) {
+        if (valor === "pow") return Math.pow(num, 2);
+        if (valor === "root") return Math.sqrt(num, 2);
+
+        return eval(this.display.value);
     }
 }
 
 const calc = new Calculator();
-calc.teste();
-calc.meuEventoDeClique();
