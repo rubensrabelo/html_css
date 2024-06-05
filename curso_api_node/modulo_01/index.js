1c5956b74c6ab03b316a148ba7ad5ec469a6600a
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-06-05 20:44:32 -0300

    Learning about Crud, Routes and Middleware

--- curso_api_node/modulo_01/index.js
+++ curso_api_node/modulo_01/index.js
@@ -5,16 +5,61 @@ app.use(express.json());
 
 const cursos = ["Node JS", "JavaScript", "React Native"];
 
-app.get("/curso", (req, res) => {
-    const nome = req.query.nome;
+app.use((req, res, next) => {
+    console.log(`Requisicao Chamada em ${req.url}`);
 
-    return res.json({ curso : `Aprendendo ${nome}`});
+    return next();
 });
 
-app.get("/curso/:index", (req, res) => {
-    const index = req.params.index;
+function checkCurso(req, res, next){
+    if(!req.body.name) 
+        return res.status(404).json({error: "O nome do curso é obrigatório."});
 
-    return res.json({ curso : `${cursos[index]}`})
+    return next();
+}
+
+function checkIndex(req, res, next){
+    const curso = cursos[req.params.index]
+
+    if(!curso)
+        return res.status(404).json({error: "O curso não existe"});
+
+    req.curso = curso;
+
+    return next();
+}
+
+app.get("/cursos", (req, res) => {
+    return res.json(cursos);
+});
+
+app.get("/cursos/:index", checkIndex, (req, res) => {
+    return res.json(req.curso)
+});
+
+app.post("/cursos", checkCurso, (req, res) => {
+    const {name} = req.body;
+    
+    cursos.push(name);
+    
+    return res.json(cursos);
+});
+
+app.put("/cursos/:index", checkCurso, checkIndex, (req, res) => {
+    const {index} = req.params;
+    const {name} = req.body;
+
+    cursos[index] = name;
+
+    return res.json(cursos);
+});
+
+app.delete("/cursos/:index", checkIndex, (req, res) => {
+    const { index } = req.params;
+
+    cursos.splice(index, 1);
+
+    return res.json(cursos);
 });
 
 app.listen(3000);
