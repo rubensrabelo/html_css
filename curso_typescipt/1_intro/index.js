6366e4c3cffb2b6bc77bf423fcd07097338a8e5f
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-06-04 22:21:23 -0300

    First Program

---
+++ curso_typescipt/1_intro/index.js
@@ -0,0 +1,6 @@
+var firstName = "Rubens";
+var anotherName = "Rebeca";
+function greetings(name) {
+    console.log("Olá " + name);
+}
+greetings(firstName);
---
+++ curso_typescipt/1_intro/index.ts
@@ -0,0 +1,8 @@
+const firstName = "Rubens";
+const anotherName = "Rebeca";
+
+function greetings(name: string){
+    console.log("Olá " + name);
+}
+
+greetings(firstName);
