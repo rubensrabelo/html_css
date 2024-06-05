8b6bcdc8c99018ca29af26a157f92ced29f4d595
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-06-05 18:42:04 -0300

    Understanding the type in TypeScript

---
+++ curso_typescipt/2_fundamentos/index.js
@@ -0,0 +1,8 @@
+var x = 10;
+console.log(x);
+// console.log(typeof x);
+var nome = "Rubens";
+console.log(nome);
+var value = false;
+console.log(value);
+var ann = "teste";
---
+++ curso_typescipt/2_fundamentos/index.ts
@@ -0,0 +1,16 @@
+let x: number = 10;
+
+console.log(x);
+
+// console.log(typeof x);
+
+let nome: string = "Rubens";
+
+console.log(nome);
+
+let value: boolean = false;
+
+console.log(value);
+
+let ann: string = "teste";
+
