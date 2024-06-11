a15adaf12dd8c533685f3e9498cdddb01ade9ba2
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-06-11 11:45:13 -0300

    Understading about import and export in the nodeJS

---
+++ curso_node/2_fundamentos/1_export_import/index.mjs
@@ -0,0 +1,5 @@
+// Sintaxe de importação de uma função soma no módulo sumFunction.mjs
+import soma from "./sumFunction.mjs";
+
+// Utilizando a função soma, passando dois argumento e mostrando o resultado no console
+console.log(soma(2, 2));
