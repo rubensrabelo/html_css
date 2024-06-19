5c57b1fb8baed1b45658a862cc122ea35720a523
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-06-19 15:28:26 -0300

    Timer program

---
+++ exercicios/model_05_timer/assets/js/script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const botaoIniciar = document.querySelector(".iniciar");
+    botaoIniciar.addEventListener("click", () => {
+        iniciar();
+    });
+    
+    const botaoPausar = document.querySelector(".pausar");
+    botaoPausar.addEventListener("click", () => {
+        pausar();
+    });
+    
+    const botaoZerar = document.querySelector(".zerar");
+    botaoZerar.addEventListener("click", () => {
+        zerar();
+    });
+});
+
+const div = document.querySelector(".resultado");
+
+let cont = 0;
+
+function iniciar(){
+}
+
+function pausar(){
+}
+
+function iniciar(){
+}
