8096ab5ca2a473edc0eac00bb7129f5f9ab27c12
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-06-20 18:35:28 -0300

    Express Program

--- curso_node/4_express/main.js
+++ curso_node/4_express/main.js
@@ -2,8 +2,19 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Seja bem-vindo!");
+const path = require("path");
+
+const basePath = path.join(__dirname, "templates");
+
+app.get("/user/:id", (req, res) => {
+    const idUser = req.params.id
+    res.send(`Seja bem-vindo, ${idUser}!`);
 });
 
-app.listen(port);
+app.get("/", (req, res) => {
+    res.sendFile(`${basePath}/index.html`);
+});
+
+app.listen(port, () => {
+    console.log(`Site rodando em http://localhost:${port}/`)
+});
