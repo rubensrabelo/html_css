2d34e19fcc8c374243b1a6be9e3010755cccc68b
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-06-20 14:44:18 -0300

    Stopwatch Program

--- exercicios/model_05_timer/assets/js/script.js
+++ exercicios/model_05_timer/assets/js/script.js
@@ -1,29 +1,60 @@
 document.addEventListener("DOMContentLoaded", () => {
     const botaoIniciar = document.querySelector(".iniciar");
     botaoIniciar.addEventListener("click", () => {
-        iniciar();
+        iniciarCronometro();
     });
     
     const botaoPausar = document.querySelector(".pausar");
     botaoPausar.addEventListener("click", () => {
-        pausar();
+        pausarCronometro();
     });
     
     const botaoZerar = document.querySelector(".zerar");
     botaoZerar.addEventListener("click", () => {
-        zerar();
+        zerarCronometro();
     });
 });
 
-const div = document.querySelector(".resultado");
+const div = document.querySelector(".timer");
+let seg = 0;
+let min = 0;
+let hora = 0;
+let intervalo;
 
-let cont = 0;
-
-function iniciar(){
+function iniciarCronometro(){
+    intervalo = setInterval(atualizarCronometro, 1000);
 }
 
-function pausar(){
+function pausarCronometro(){
+    clearInterval(intervalo);
 }
 
-function iniciar(){
+function zerarCronometro(){
+    pausarCronometro();
+    seg = 0;
+    min = 0;
+    hora = 0;
+    exibirNaTela();
+}
+
+function atualizarCronometro(){
+    seg++;
+    if(seg === 60){
+        min++;
+        seg = 0;
+    };
+    if(min === 60) {
+        min = 0;
+        hora++
+    };
+
+    exibirNaTela();
+}
+
+function exibirNaTela(){
+    div.innerHTML = `${formatarTempo(hora)}:${formatarTempo(min)}:${formatarTempo(seg)}`
+}
+
+function formatarTempo(tempo) {
+    return tempo < 10 ? `0${tempo}` : tempo;
 }
