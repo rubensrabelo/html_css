ac576d29ab4d8991a458e018a0859402e0ff137b
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-06-22 11:14:53 -0300

    Templates with partials

--- curso_node/06_template_engine/index.js
+++ curso_node/06_template_engine/index.js
@@ -3,8 +3,37 @@ const exphbs = require("express-handlebars");
 
 const app = express(); 
 
-app.engine("handlebars", exphbs.engine());
+const hbs = exphbs.create({
+    partialsDir: ["views/partials"],
+});
+
+app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+
+
+
+app.get("/dashboard", (req, res) => {
+    const items = ["item 1", "item 2", "item 3", "item 4", "item 5"];
+    const user = {
+        name: "Rubens",
+        surname: "Rabelo"
+    }
+
+    res.render("dashboard", {items, user});
+});
+
+app.get("/blog", (req, res) => {
+    const posts = [
+        {
+            title: "Aprender Node.js",
+            category: "Javascript",
+            body: "teste",
+            comments: 4,
+        }
+    ];
+
+    res.render("blog", {posts});
+});
 
 app.get("/", (req, res) => {
     const user = {
@@ -12,7 +41,9 @@ app.get("/", (req, res) => {
         surname: "Rabelo"
     }
 
-    res.render("home", {user: user});
+    const auth = true;
+
+    res.render("home", {user, auth});
 });
 
 app.listen(3000, () => {
