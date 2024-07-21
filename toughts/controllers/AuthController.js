87f681f52e56bba730e17a15de108438ec067717
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-07-21 16:23:38 -0300

    Creating the user

--- toughts/controllers/AuthController.js
+++ toughts/controllers/AuthController.js
@@ -1,3 +1,7 @@
+const User = require("../models/User");
+
+const bcrypt = require("bcryptjs");
+
 module.exports = class AuthController {
     static login(req, res) {
         res.render("auth/login");
@@ -5,5 +9,46 @@ module.exports = class AuthController {
 
     static register(req, res) {
         res.render("auth/register");
+    }
+
+    static async registerPost(res, req) {
+        const {name, email, password, confirmPassword} = req.body;
+
+        if(password != confirmPassword){
+            req.flash("message", "The passwords don't match, try again!");
+            res.render("auth/register");
+            return;
+        }
+
+        const checkIfUserExists = await User.findOne({where: {email: email}});
+
+        if(checkIfUserExists) {
+            req.flash("message", "The email is already in use");
+            res.render("auth/register");
+            return;
+        }
+
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(password, salt);
+
+        const user = {
+            name,
+            email,
+            password: hashedPassword
+        };
+
+        try {
+            const createUser = await User.create(user);
+            req.flash("message", "Registration completed successfully");
+
+            req.session.userid = createUser.id;
+
+            req.session.save(() => {
+                res.redirect("/");
+            });
+
+        } catch (err) {
+            console.log(err);
+        }
     }
 }
