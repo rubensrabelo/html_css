6e7d500c32ab77c5f5d31edd7c81bb970af078d7
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-07-26 13:00:11 -0300

    Editing home

--- toughts/controllers/ToughtController.js
+++ toughts/controllers/ToughtController.js
@@ -3,7 +3,13 @@ const User = require("../models/User");
 
 module.exports = class ToughtController {
     static async showToughts(req, res) {
-        res.render("toughts/home");
+        const toughtsData = await Tought.findAll({
+            include: User,
+        });
+
+        const toughts = toughtsData.map((result) => result.get({plain: true}));
+
+        res.render("toughts/home", {toughts});
     }
 
     static async dashboard(req, res) {
@@ -21,6 +27,12 @@ module.exports = class ToughtController {
         }
 
         const toughts = user.Tought.map((result) => result.dataValues);
+
+        let emptyToughts = false;
+
+        if(toughts.length === 0){
+            emptyToughts = true;
+        }
 
         res.render("toughts/dashboard", {toughts});
     }
@@ -62,6 +74,34 @@ module.exports = class ToughtController {
             });
         } catch(err) {
             console.log(err);
+        }
+    }
+
+    static async updateTought(req, res) {
+        const id = req.params.id;
+
+        const tought = await Tought.findOne({where: {id: di}, raw: true});
+
+        res.render("toughts/edit", {tought});
+    }
+
+    static async updateToughtSave(req, res) {
+        const id = rq.body.id;
+
+        const tought = {
+            title: req.body.title,
+        }
+
+        try {
+            await Tought.update(tought, {where: {id: id}});
+
+            req.flash("message", "Tought successfully updated");
+    
+            req.session.save(() => {
+                res.redirect("/toughts/dashboard");
+            });
+        } catch (error) {
+            console.log(error);
         }
     }
 }
--- toughts/router/toughtRoutes.js
+++ toughts/router/toughtRoutes.js
@@ -5,8 +5,10 @@ const checkAuth = require("../helpers/auth").checkAuth;
 
 const router = express.Router();
 
-router.get("/add", checkAuth, ToughtController.createTought)
-router.post("/add", checkAuth, ToughtController.createToughtsSave)
+router.get("/add", checkAuth, ToughtController.createTought);
+router.post("/add", checkAuth, ToughtController.createToughtsSave);
+router.get("/edit/:id", checkAuth, ToughtController.updateTought);
+router.post("/edit", checkAuth, ToughtController.updateToughtSave);
 router.get("/dashboard", checkAuth, ToughtController.dashboard);
 router.post("/remove", checkAuth, ToughtController.removeTought);
 router.get("/", ToughtController.showToughts);
