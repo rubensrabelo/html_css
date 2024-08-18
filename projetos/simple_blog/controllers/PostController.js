814b01e40784a70123a8bec0182165b321e3e915
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-08-18 14:45:49 -0300

    Ending simple blog project

--- projetos/simple_blog/controllers/PostController.js
+++ projetos/simple_blog/controllers/PostController.js
@@ -21,4 +21,33 @@ module.exports = class PostController {
 
         res.redirect("/posts");
     }
+
+    static async editPostsGet(req, res) {
+        const id = req.params.id;
+
+        const post = await Post.findOne({where: {id}, raw: true});
+
+        res.render("posts/edit", {post});
+    }
+
+    static async editPosts(req, res) {
+        const id = req.body.id;
+
+        const post = {
+            title: req.body.title,
+            comment: req.body.comment
+        };
+
+        await Post.update(post, {where: {id}});
+
+        res.redirect("/posts");
+    }
+
+    static async deletePost(req, res) {
+        const id = req.body.id;
+
+        await Post.destroy({where: {id}});
+
+        res.redirect("/posts");
+    }
 }
--- projetos/simple_blog/router/postRouter.js
+++ projetos/simple_blog/router/postRouter.js
@@ -4,6 +4,9 @@ const PostController = require("../controllers/PostController");
 
 const router = express.Router();
 
+router.post("/delete", PostController.deletePost)
+router.post("/edit", PostController.editPosts)
+router.get("/edit/:id", PostController.editPostsGet)
 router.post("/create", PostController.createPosts)
 router.get("/create", PostController.createPostsGet)
 router.get("/", PostController.showAllPosts);
