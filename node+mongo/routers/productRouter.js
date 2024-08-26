f15238ca4210b6d7308a8e31efcb14d7adfa9f25
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-08-26 18:30:44 -0300

    Implementing create method

--- node+mongo/routers/productRouter.js
+++ node+mongo/routers/productRouter.js
@@ -4,6 +4,8 @@ const ProductController = require("../controllers/ProductController");
 
 const router = express.Router();
 
+router.get("/create", ProductController.createProduct);
+router.post("/create", ProductController.createProductPost);
 router.get("/", ProductController.showProducts);
 
 module.exports = router;
