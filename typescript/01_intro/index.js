782f4dad67a06a455414de07fc088f500b649bdf
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-09-01 09:38:50 -0300

    Removin typescript folder

--- typescript/01_intro/index.js
+++
@@ -1,7 +0,0 @@
-var firstName = "Rubens";
-var anotherName = 1;
-var x = true;
-function greetings(name) {
-    console.log("Olá " + name);
-}
-greetings(firstName);
--- typescript/01_intro/index.ts
+++
@@ -1,9 +0,0 @@
-const firstName = "Rubens";
-const anotherName = 1;
-const x = true;
-
-function greetings(name: string) {
-    console.log("Olá " + name);
-}
-
-greetings(firstName);
