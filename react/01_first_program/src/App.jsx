4c0a6134f0429a5f72fef3a56f9a609e8cdd7495
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-09-03 14:40:57 -0300

    First program with react

---
+++ react/01_first_program/src/App.jsx
@@ -0,0 +1,17 @@
+import FirstComponent from "./components/FirstComponent"
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>First Program</h1>
+      <p>Welcome to my first program in React!</p>
+
+      <FirstComponent />
+    </>
+  )
+}
+
+export default App
