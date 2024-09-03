a6fdd489fb872db94ab9a8a165d363e2f967752e
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-09-03 12:43:19 -0300

    User authorization with jwt and express

--- get_a_pet/backend/controllers/UserController.js
+++ get_a_pet/backend/controllers/UserController.js
@@ -1,7 +1,9 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 const createUserToken = require("../helpers/create-user-token");
+const getToken = require("../helpers/get-token");
 
 module.exports = class UserController {
     
@@ -63,5 +65,65 @@ module.exports = class UserController {
         } catch (error) {
             res.status(500).json({ message: error });
         }
+    }
+
+    static async login(req, res) {
+        const {email, password} = req.body;
+
+        if(!email) {
+            res.status(422).json({message: "Enter email!"});
+            return;
+        }
+
+        if(!password) {
+            res.status(422).json({message: "Enter password!"});
+            return;
+        }
+
+        const user = await User.findOne({email: email});
+
+        if(!user) {
+            res.status(422).json({ message: "There is no registered user with this email!"});
+            return;
+        }
+
+        const checkPassword = await bcrypt.compare(password, user.password);
+
+        if(!checkPassword) {
+            res.status(422).json({ message: "Invalid Password"});
+            return;
+        }
+        
+        await createUserToken(user, req, res);
+    }
+
+    static async checkUser(req, res) {
+        let currentUser;
+
+        if(req.headers.authorization) {
+            const token = getToken(req);
+            const decoded = jwt.verify(token, "nossosecret");
+
+            currentUser = await User.findById(decoded.id);
+
+            currentUser.password = undefined;
+        } else {
+            currentUser = null;
+        }
+
+        res.status(200).send(currentUser);
+    }
+
+    static async getUserById(req, res) {
+        const id = req.params.id;
+
+        const user = await User.findById(id).select("-password");
+
+        if(!user){
+            res.status(422).json({message: "User not found!"});
+            return;
+        }
+
+        res.status(200).json({ user });
     }
 }
