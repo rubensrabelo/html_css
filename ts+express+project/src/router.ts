9eb701af37c1a1f3b8f42948d6f1d93be86850e8
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-09-29 11:01:40 -0300

    Removing folders

--- ts+express+project/src/router.ts
+++
@@ -1,9 +0,0 @@
-import {Router, Request, Response} from "express";
-
-import { createMovie } from "./controllers/MovieController";
-
-const router = Router();
-
-export default router.get("/test", (req: Request, res: Response) => {
-    res.status(200).send("API working!");
-}).post("/movie", createMovie);
