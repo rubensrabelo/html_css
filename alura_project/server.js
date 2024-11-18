edb986e28483a318deb8a6986b9078cc4f04615c
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-18 16:22:02 -0300

    Finishing first class

--- alura_project/server.js
+++ alura_project/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/api", (req, res) => {
+    res.status(200).send("A Torre Eiffel iluminada à noite, com milhares de luzes cintilando, criando um espetáculo mágico em Paris.");
+});
+
 app.listen(3000, () => {
     console.log("Server is running!");
 });
