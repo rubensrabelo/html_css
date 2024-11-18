91bba05288f8bf73809f97d9a4a747429635534a
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-18 16:28:34 -0300

    Finishing book route exercise

--- alura_project/server.js
+++ alura_project/server.js
@@ -6,6 +6,17 @@ app.get("/api", (req, res) => {
     res.status(200).send("A Torre Eiffel iluminada à noite, com milhares de luzes cintilando, criando um espetáculo mágico em Paris.");
 });
 
+app.get("/api/book", (req, res) => {
+    const book = {
+        nome: "Imaginário",
+        autor: "Sonhadro",
+        anoPublicacao: 2024,
+        genero: "Fantasia"
+    };
+
+    res.status(200).send(book);
+});
+
 app.listen(3000, () => {
     console.log("Server is running!");
 });
