92225fb616e35bb9a58da8fc33560556ae2760fd
Rubens_Rabelo <rubensrabelo123@gmail.com>
2024-11-20 12:26:56 -0300

    Starting second class in alura project

--- alura_project/server.js
+++ alura_project/server.js
@@ -2,19 +2,34 @@ import express from "express";
 
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.status(200).send("A Torre Eiffel iluminada à noite, com milhares de luzes cintilando, criando um espetáculo mágico em Paris.");
+const posts = [
+    { id: 1, descricao: "Uma foto teste", imagem: "https://thispersondoesnotexist.com/" },
+    { id: 2, descricao: "Um momento inesquecível", imagem: "https://thispersondoesnotexist.com/" },
+    { id: 3, descricao: "Explorando novos horizontes", imagem: "https://thispersondoesnotexist.com/" },
+    { id: 4, descricao: "Curtindo a vida ao máximo", imagem: "https://thispersondoesnotexist.com/" },
+    { id: 5, descricao: "Um dia cheio de aprendizados", imagem: "https://thispersondoesnotexist.com/" },
+    { id: 6, descricao: "Vivendo o agora", imagem: "https://thispersondoesnotexist.com/" },
+    { id: 7, descricao: "Mais um dia especial", imagem: "https://thispersondoesnotexist.com/" },
+    { id: 8, descricao: "Um clique perfeito", imagem: "https://thispersondoesnotexist.com/" },
+    { id: 9, descricao: "Momentos que importam", imagem: "https://thispersondoesnotexist.com/" },
+    { id: 10, descricao: "Lembranças que ficam", imagem: "https://thispersondoesnotexist.com/" }
+];
+
+
+app.use(express.json());
+
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
 });
 
-app.get("/api/book", (req, res) => {
-    const book = {
-        nome: "Imaginário",
-        autor: "Sonhadro",
-        anoPublicacao: 2024,
-        genero: "Fantasia"
-    };
+app.get("/posts/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find(p => p.id === id);
 
-    res.status(200).send(book);
+    if(post)
+        res.status(200).json(post);
+    else
+        res.status(404).json({error: "Post não encontrado!"});
 });
 
 app.listen(3000, () => {
